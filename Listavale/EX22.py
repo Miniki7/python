0698532b9782bfa64d15cdf5e951159f7859ee2a
Mirela Tiscoski Pasini <133065402+Miniki7@users.noreply.github.com>
2023-08-21 20:30:52 -0300

    Update EX22.py

--- Listavale/EX22.py
+++ Listavale/EX22.py
@@ -1,6 +1,13 @@
 '''22) Crie um programa que leia vários números inteiros pelo teclado. O programa só pode para quando for
 digitado o numero 1000. No final, mostre quantos números foram digitados e qual foi a soma deles.
 Desconsiderando o valor 1000 da parada.'''
-i = 0
-for i in i!=1000:
-    n= int()
+s = 0
+q = 0
+while True:
+   n = int(input("Digite um número inteiro: "))
+   if n == 1000:
+      break
+   s = s + n
+   q = q + 1
+print(f'A quantidade de números digitados foi: {q}')
+print(f'A soma dos numeros digitados foi {s}')
